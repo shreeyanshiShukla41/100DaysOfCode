7c43c410ce2d2f9e68ecbb9cf839420a612066d4
shreeyanshiShukla41 <shreeyanshishukla41@gmail.com>
2026-05-27 23:34:35 +0530

    update

--- Day01/Dsa1.java
+++ Day01/Dsa1.java
@@ -22,5 +22,13 @@ public class Dsa1{
         }
         return arr;
     }
+
+    // nth fibbonacci number 27-05-2026
+     public int fib(int n) {
+        if(n==0 || n==1) return n;
+        else{
+            return fib(n-1)+fib(n-2);
+        }
+    }
   }
 }
