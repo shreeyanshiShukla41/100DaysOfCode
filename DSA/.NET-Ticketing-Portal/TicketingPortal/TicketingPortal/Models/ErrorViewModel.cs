7c12d5bce19c23f72ac6367fb0b2675d968f00d0
shreeyanshiShukla41 <shreeyanshishukla41@gmail.com>
2026-06-05 11:26:36 +0530

    Initial commit - Clean 100DaysOfCode structure

--- DSA/.NET-Ticketing-Portal/TicketingPortal/TicketingPortal/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TicketingPortal.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
