2c58bf63d6777a2c502af47420c306439f0a75f4
shreeyanshiShukla41 <shreeyanshishukla41@gmail.com>
2026-06-05 10:51:43 +0530

    Fix DSA tracking and convert to regular folder

---
+++ DSA/.NET-Ticketing-Portal/TicketingPortal/TicketingPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net.Sockets;
+using TicketingPortal.Models;
+
+namespace TicketingPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
---
+++ DSA/README.md
@@ -0,0 +1,2 @@
+# 100DaysOfCode
+A 100-day coding commitment focused on mastering core Data Structures & Algorithms (DSA) and building scalable full-stack applications using .NET Core, React.js, and the MERN stack. Tracking daily progress, problem-solving consistency, and clean code practices.
